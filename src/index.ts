c2f7243ad769f2d26f7d3169dcac5ff77edb49ec
svonme <svon.me@gmail.com>
2025-12-01 13:17:50 +0800

    feat: 导出内容管理

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@
 export {useEvent} from "./sheet/event";
 export {CellEventName} from "./sheet/register";
 export {merge as CellMerge} from "./sheet/merge";
-export {StyleValue, SheetConfig, MenuEvent} from "./sheet/config";
-export type {ColumnList, RowList, ContextMenu} from "./types/prop";
+export {StyleValue, SheetConfig, MenuEvent, GetCell} from "./sheet/config";
 export {CellType, OptionKV, Column, Cell, EditCellData} from "./types/sheet";
 export {fillGenerateValue as fillGenerate, fillCellCompute} from "./sheet/fill";
 
 export {default as Sheet} from "./sheet/sheet.vue";
 
+export type {ColumnList, RowList, ContextMenu} from "./types/prop";
 export type {Row, RowBasis, RowCells, FillCellOption} from "./types/sheet";
